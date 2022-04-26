8a7f3cea904f326f0bebefc7ef084da8dd49ca55
Rúni Gudmundarson <78358597+Raygud@users.noreply.github.com>
2022-04-26 12:27:13 +0200

    Add files via upload

--- Place.js
+++ Place.js
@@ -1,33 +1,24 @@
-let x = "Red";
-let paint = "Red";
-
+let a = false
+let UserExsists;
 
 let Users = [
-    "2dada",
-     "dsaasd",
-     "Red",
-     "adsas",
-     "Red",
-     "raygud",
-     "Red",
-     "aaasdd",
-     "Red"
-]
-
-let UsersInfo = [
     {Name:"2dada",
      Color: "Red"},
-     {Name:"dsaasd",
+     {Name:"sadsd",
      Color: "Red"},
      {Name:"adsas",
      Color: "Red"},
-     {Name:"raygud",
+     {Name:"dd",
      Color: "Red"},
      {Name:"aaasdd",
      Color: "Red"},
 ]
 
-for(let i = 1; i <= 5000 ; i++){
+
+
+
+
+for(let i = 0; i <= 5049 ; i++){
 const node = document.createElement("td");
 document.getElementById("PlaceContainer").appendChild(node);
 node.setAttribute("id", "Place"+i);
@@ -46,30 +37,56 @@ client.on('message', (channel, tags, message, self) => {
     console.log(tags.username + ": " + message)
 
     for(let i = 0; i < Users.length; i++){
-        if(message.includes("Color") && tags.username.toLocaleLowerCase() == Users[i].Name.toLocaleLowerCase()){
+        if(!Users[i].Name.includes(tags.username)){
             
-            Users[i].Color = message.substr(6,message.length+1)
-            console.log(message.substr(6,message.length+1))
-            paint = Users[i].Color 
-            
+            console.log(!Users[i].Name.includes(tags.username)+" "+Users[i].Name)
+            if(!Users[i].Name.includes(tags.username)){
+                console.log("User: " + tags.username + " not found")
+                a = false
+            }
+        }
+        else{
+            console.log("User found at Users[" + i + "].Name") 
+            a = true
+            break;
         }
     }
-    console.log(x)
-
-    if(!Users.includes(tags.username)){
-        UsersInfo.push({Name:tags.username,
-                    Color: x})
-                    console.log(UsersInfo)
+    if(a == true){
+        console.log("osdko")
+    }
+    if(a == false){
+        console.log("added")
+        Users.push({Name: tags.username,
+                          Color: "Red"})
+                          console.log(Users)
     }
     
-    if(message.startsWith("P".toLocaleLowerCase()) || message.startsWith("P"))
-    {
 
-        console.log("P"+message.substr(0,0))
-        document.getElementById("Place"+message.substr(1,message.length)).style.backgroundColor = paint
-        
+
+    if(message.startsWith("P".toLocaleLowerCase()) && message.replace(/\s+/g, '').length < 6 ){
+        console.log(message.substr(1,message.length))
+        tags.username
+        let indexOfUser = Users.map(object => object.Name).indexOf(tags.username);
+
+        document.getElementById("Place"+message.substr(1,message.length)).style.backgroundColor = Users[indexOfUser].Color
     }
- 
-    console.log(Users)
+
+    if(message.toLocaleLowerCase().substr(0,5) == "Color".toLocaleLowerCase()){
+        console.log(message.toLocaleLowerCase().substr(0,5))
+
+        ChangeColor(tags.username,message.replace(/\s+/g, '').substr(5,message.length))
+    }
+    
+    
+    
 });
+
+function ChangeColor(User,Color){
+
+    let indexOfUser = Users.map(object => object.Name).indexOf(User);
+    console.log(indexOfUser)
+
+    Users[indexOfUser].Color = Color
+
+}
 					
